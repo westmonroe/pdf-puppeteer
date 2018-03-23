9f4790e02a838742ff4f3cb49980488b05e7d89f
Joe Higgs <joebh5@gmail.com>
2018-03-23 14:30:04 -0500

    Clean up

--- src/convertHTMLToPDF.js
+++ src/convertHTMLToPDF.js
@@ -6,7 +6,6 @@ let convertHTMLToPDF = async (html, callback, options = null) => {
   if (!options) {
     options = { format: "letter" };
   }
-  console.log(html)
   await page.setContent(html);
   await page.pdf({ format: "letter" }).then(callback);
   await browser.close();
