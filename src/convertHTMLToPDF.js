5ac45c5caee9f5cd967ccf289ceb6d550a89b636
Joe Higgs <jhiggs@westmonroepartners.com>
2018-03-23 11:37:08 -0500

    Lightly testde function

--- src/convertHTMLToPDF.js
+++ src/convertHTMLToPDF.js
@@ -1,17 +1,15 @@
 const puppeteer = require('puppeteer');
 
-async function convertHTMLToPDF (html, options=null) {
+async function convertHTMLToPDF (html, callback, options=null) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     if (!options) {
         options = { format: 'letter' }; 
     }
     await page.setContent(html);
-    await page.pdf(options)
-    .then(function (pdf) {
-        await browser.close();
-        return pdf;
-    });
-}
+    await page.pdf({ format: 'letter' })
+    .then(callback);
+    await browser.close();
+  }
 
 export default convertHTMLToPDF;
