ec6dfe2a368dd903bb32aa0f55d86418541dde2b
Joe Higgs <joebh5@gmail.com>
2018-06-11 11:38:56 -0500

    Updating version and code to fix blank pdf bug caused by external resources

--- src/convertHTMLToPDF.js
+++ src/convertHTMLToPDF.js
@@ -6,7 +6,18 @@ let convertHTMLToPDF = async (html, callback, options = null) => {
     if (!options) {
         options = { format: 'Letter' };
     }
-    await page.setContent(html);
+    
+    // From https://github.com/GoogleChrome/puppeteer/issues/728#issuecomment-359047638
+    // Using this method to preserve external resources while maximizing allowed size of pdf
+    // Capture first request only
+    await page.setRequestInterception(true);
+    page.once('request', request => {
+      // Fulfill request with HTML, and continue all subsequent requests
+      request.respond({body: html});
+      page.on('request', request => request.continue());
+    });
+    await page.goto('http://example.com');
+    
     await page.pdf(options).then(callback, function(error) {
         console.log(error);
     });
