6d2aae265e5bd918e97a111ca4a8ce7b68660a13
Joe <joebh5@gmail.com>
2018-08-19 12:37:04 -0500

    Updating error handling and examples

--- src/convertHTMLToPDF.js
+++ src/convertHTMLToPDF.js
@@ -1,23 +1,27 @@
 const puppeteer = require('puppeteer');
 
 let convertHTMLToPDF = async (html, callback, options = null) => {
+    if (typeof html !== 'string') {
+        throw new Error(
+            'Invalid Argument: HTML expected as type of string and received a value of a different type. Check your request body and request headers.'
+        );
+    }
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     if (!options) {
         options = { format: 'Letter' };
     }
-    
     // From https://github.com/GoogleChrome/puppeteer/issues/728#issuecomment-359047638
     // Using this method to preserve external resources while maximizing allowed size of pdf
     // Capture first request only
     await page.setRequestInterception(true);
     page.once('request', request => {
-      // Fulfill request with HTML, and continue all subsequent requests
-      request.respond({body: html});
-      page.on('request', request => request.continue());
+        // Fulfill request with HTML, and continue all subsequent requests
+        request.respond({ body: html });
+        page.on('request', request => request.continue());
     });
-    await page.goto('http://example.com');
-    
+    await page.goto('https://google.com');
+
     await page.pdf(options).then(callback, function(error) {
         console.log(error);
     });
