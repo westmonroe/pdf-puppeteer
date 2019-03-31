ffb02bd2bb2d22a3d4051fa1e9fee4b5755a65dd
Joe <joebh5@gmail.com>
2019-03-31 17:17:46 -0500

    update example with custom name info

--- example/index.js
+++ example/index.js
@@ -1,5 +1,4 @@
 const express = require('express'),
-    puppeteer = require('puppeteer'),
     bodyParser = require('body-parser'),
     http = require('http'),
     convertHTMLToPDF = require('pdf-puppeteer');
@@ -7,17 +6,39 @@ const express = require('express'),
 const app = express();
 const router = express.Router();
 
+// prevent cors issue for the test html by file ref
+// don't use in production
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With,[content-type]'
+    );
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 // Example PDF route, can be used with the postman profile
 // to test different option configurations, for example:
 // changing the remoteContent option to false will allow the "GiantPDF"
 // to load faster, but without the remote images.
 router.route('/pdf').post(async function(req, res) {
-    convertHTMLToPDF(req.body, pdf => {
-        res.setHeader('Content-Type', 'application/pdf');
-        res.send(pdf);
-    }, null, null, true).catch(err => {
+    // to download to loacl filesystem with custom name pass a
+    // "path" option. see the page.pdf docs for more info
+    // https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
+    convertHTMLToPDF(
+        req.body,
+        pdf => {
+            res.setHeader('Content-Type', 'application/pdf');
+            res.send(pdf);
+        },
+        null,
+        null,
+        true
+    ).catch(err => {
         console.log(err);
-        res.status(500).send('An error occurred');
+        res.status(500).send(err);
     });
 });
 
